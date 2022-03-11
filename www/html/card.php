f4c09f7d9c3232a1c85c54be7b25e30b59f43b65
Justin Dieser <justindieser@MacBook-Pro-2.local>
2022-03-11 11:43:09 +0100

    Update card.php

--- www/html/card.php
+++ www/html/card.php
@@ -1,6 +1,6 @@
 <div class="card">
   <div class="card-titel"><?= $row['Name']?></div>
-  <hr>
+  <br>
   <img src="\assets\pictures\pexels-nana-dua-8622912.jpg"  class="card h-20" class="card-img-top">
   <div class="card-body">
 
