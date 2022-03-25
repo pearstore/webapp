a63308a9b35ce31c3d0a5165732c43b595259393
xWilliam <44903208+xWilli4m@users.noreply.github.com>
2022-03-25 11:58:12 +0100

    ware.php und card.php brauchbar gemacht

--- www/html/card.php
+++ www/html/card.php
@@ -1,12 +1,12 @@
-<div class="card" >
+<div class="card mb-3" >
   <div class="card-titel"><?= $row['Name']?></div>
   <br>
-  <img src="\assets\pictures\pexels-nana-dua-8622912.jpg" width="300" height="200"  class="card h-20" class="card-img-top">
+  <img src="\assets\pictures\<?= $row['Name'] ?>.jpg" class="card h-20" class="card-img-top">
   <div class="card-body">
 
-<?= $row['Beschreibung'] ?>
-<hr>
-<?= $row['Preis']?>
+  <?= $row['Beschreibung'] ?>
+  <hr>
+  <?= $row['Preis']?>
   </div>
   <div class="card-footer">
       <a href="" class="btn btn-primary btn-sm">Details</a>
