ead20a6c96d6e8d109fd12f8192744c97312d7ef
Justin Dieser <justindieser@MacBook-Pro-2.local>
2022-05-24 11:21:51 +0200

    Update navbar.php
    Navbar edit icon

--- www/html/navbar.php
+++ www/html/navbar.php
@@ -3,15 +3,15 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container" id="navbarToggleExternalContent">
-            <a class="navbar-brand" href="/">PearStore</a>
+          <a class="navbar-brand" href="/">
+            <img src="/assets/pictures/icon/Pear.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+            Pearstore
+          </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarScroll">
                 <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="/">Home</a>
-                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/Ware.php">Ware</a>
                     </li>
@@ -228,5 +228,5 @@
     <?php endif; ?>
     <!-- Alerts - Ende -->
 
-    
+
 </div>
