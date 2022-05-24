1956142e647f0f8b910cd808ce19e6d6fba9e8aa
Lunalena <52126362+Lunalena@users.noreply.github.com>
2022-05-24 11:14:08 +0200

    Added a Cookie popup that loads on every page open

---
+++ www/html/cookie_popup.php
@@ -0,0 +1,27 @@
+
+
+
+
+
+<div class="modal fade" id="Cookie" tabindex="-1" aria-labelledby="Cookielabel">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Cookies !</h5>
+      </div>
+      <div class="modal-body">
+        <p>We use Cookies to enhance your Expierence<br> and to make sure we know who you are<br>
+		Please let us use Cookies</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Accept</button>
+        <button type="button" class="btn btn-primary">Submit</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
--- www/html/index.php
+++ www/html/index.php
@@ -17,18 +17,27 @@ $result = mysqli_query($link,$sql);
     <link rel="icon" type="image/png" href="/assets/pictures/icon/icon.png">
     <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="/assets/css/styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 </head>
 
 
 <body>
     <?php require_once("navbar.php"); ?>
-    <div class="container pt-5">
+    <div>
         <h2>Willkommen bei Pear</h2>
     </div>
+	<?php require_once("cookie_popup.php"); ?>
     <script src="/assets/js/bootstrap.bundle.min.js"></script>
-    <script src="/assets/js/vue.js"></script>
     <script>
         new bootstrap.Modal(document.getElementById('loginModal'));
     </script>
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs="
+  crossorigin="anonymous">
+  </script>
+  <script type="text/javascript">
+    $(window).on('load', function() {
+        $('#Cookie').modal('show');
+    });
+</script>
 </body>
 </html>
